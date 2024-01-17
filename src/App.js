c81e54eb599e362295929bd386c9d72bec4337d5
shreesleeps <shreesleeps@gmail.com>
2024-01-17 14:54:25 +0530

    Ver 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./components/home";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="bg-[#BDBDBD] w-screen h-screen">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/Block1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Block1() {
+  return (
+    <div className='w-full bg-violet-300 h-screen pt-[102px]'>Block1</div>
+  )
+}
+
+export default Block1
