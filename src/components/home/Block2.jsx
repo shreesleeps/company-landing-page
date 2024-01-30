7239012ec5baca3ffc2c4bc8a7a1ccc5ba37135e
shreesleeps <shreesleeps@gmail.com>
2024-01-30 18:49:29 +0530

    final-ver

--- src/components/home/Block2.jsx
+++ src/components/home/Block2.jsx
@@ -1,32 +1,63 @@
 import React from "react";
-import i3 from "./P3.png";
-import i4 from "./P4.png";
-import i5 from "./P5.png";
+import { LuMoveRight } from "react-icons/lu";
+import i3 from "../images/P3.jpg";
+import i4 from "../images/P4.jpg";
+import i5 from "../images/P5.jpg";
+import { useNavigate } from "react-router-dom";
 
-function Block2() {
+function Block2({ scrollToTop }) {
+  const navigate = useNavigate();
+
   return (
-    <div className="w-full bg-green-600 h-screen py-[102px] flex flex-row">
-      <div className="bg-orange-400 flex-grow flex flex-row  ">
-        <div className="bg-red-200 flex flex-col h-full w-1/2">
-          <img className="w-full h-full pl-[100px] pt-[30px] pr-[30px] pb-[30px]" src={i3} alt="img" />
-          <img className="w-full h-full pl-[100px] pr-[30px] "src={i5} alt="img" />
+    <div className="w-full h-[calc(100vh-100px)] py-[50px]">
+      <div className="h-full w-full bg-[#F3F3F3] flex flex-row px-[70px] py-[30px]">
+        <div className="h-full w-[60%] flex flex-row pl-[30px] gap-[30px]">
+          <div className="h-full w-1/2 flex flex-col">
+            <img
+              key={"fe"}
+              className="object-cover h-[65%] w-full hover:scale-105 an"
+              src={i3}
+              alt="img"
+            />
+            <img
+              key={"sd"}
+              className="object-cover h-[35%] w-full translate-y-[30px] hover:scale-105 an"
+              src={i4}
+              alt="img2"
+            />
+          </div>
+          <div className="h-full w-1/2 pt-[30px]">
+            <img
+              key={"fce"}
+              className="object-cover h-full w-full hover:scale-105 an"
+              src={i5}
+              alt="img"
+            />
+          </div>
         </div>
-        <div className="bg-red-400 h-full w-1/2">
-          <img className="w-full h-full pt-[60px] pb-[30px] flex justify-end" src={i4} alt="img" />
-        </div>
-      </div>
-      <div className="bg-green-100 w-[500px] flex flex-col ">
-        <div className="pl-[30px] pt-[30px] pr-[302px] pb-[20px]">ABOUT</div>
-        <div className="pl-[30px] pr-[70px]">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </div>
-        <div className="bg-green-100 pl-[30px] pt-[20px]">
-          <button>READ MORE</button>
+        <div className="h-full w-[40%] flex flex-col items-start justify-between pl-[30px]">
+          <div className="h1">About</div>
+          <div className="w-full h-[calc(100%-174px)] overflow-y-clip">
+            <div className="w-full h-full text-ellipsis text-wrap txt">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </div>
+          </div>
+
+          <button
+            onClick={() => {
+              scrollToTop();
+              navigate("/about");
+            }}
+            className="bg-[white] w-max h-[70px] pl-[20px] pr-[50px] flex flex-row items-center gap-[18px] border border-[white] hover:border-[#d4d4d4] group an"
+          >
+            <p className="b1">Read More</p>
+            <LuMoveRight className="text-[#333333] group-hover:translate-x-2 an" />
+          </button>
         </div>
       </div>
     </div>
--- src/components/home/Block3.jsx
+++ src/components/home/Block3.jsx
@@ -2,22 +2,26 @@ import React from "react";
 
 function Block3() {
   return (
-    <div className="bg-red-700 h-screen w-full flex flex-col justify-start py-[102px]">
-      <div className="bg-pink-300 h-[30%] flex ">Main Focus/Mission Statement</div>
-      <div className="bg-pink-500 h-[70%] flex flex-row justify-between ">
-        <div className="bg-pink-100 w-50% h-full flex flex-row gap-[30px]">
-          <div>1</div>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            efficitur, lectus et facilisis placerat.
+    <div className="w-full h-max py-[50px]">
+      <div className="h-full w-full flex flex-col">
+        <div className="h1">Main Focus/Mission Statement</div>
+        <div className="h-max pt-[40px] flex flex-row gap-[30px]">
+          <div className="h-max w-1/2 flex flex-row justify-start items-center gap-[30px]">
+            <div className="hBig w-[104px]">1</div>
+            <div className="flex-grow txtBig">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              efficitur, lectus et facilisis placerat, magna mauris porttitor
+              tortor.
+            </div>
           </div>
-        </div>
-        <div className="bg-pink-50 w-50% h-full flex flex-row gap-[30px] ">
-        <div>2</div>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            efficitur, lectus et facilisis placerat.
-          </div>
+          <div className="h-max w-1/2 flex flex-row justify-start items-center gap-[30px]">
+            <div className="hBig w-[120px]">2</div>
+            <div className="flex-grow txtBig">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              efficitur, lectus et facilisis placerat, magna mauris porttitor
+              tortor.
+            </div>
+          </div>{" "}
         </div>
       </div>
     </div>
