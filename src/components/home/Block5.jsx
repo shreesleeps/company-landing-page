d76194f25735cf5c7ab88f3d758991372b578bb0
shreesleeps <shreesleeps@gmail.com>
2024-02-17 18:06:04 +0530

    responsive ver1

--- src/components/home/Block5.jsx
+++ src/components/home/Block5.jsx
@@ -27,12 +27,12 @@ function TextInput(
                 onChange={(e) => {
                   onChange(e.target.value);
                 }}
-                // onFocus={() => {
-                //   setActive(false);
-                // }}
-                // onBlur={() => {
-                //   setActive(true);
-                // }}
+                onFocus={() => {
+                  setActive(false);
+                }}
+                onBlur={() => {
+                  setActive(true);
+                }}
                 placeholder=""
                 className="bg-[#F3F3F3] tracking-[0.56px] pTxt outline-none h-[46px] px-[20px] py-[14px] flex items-center w-full"
               />
@@ -96,10 +96,10 @@ function Block5() {
 
   return (
     <div className="w-full h-max py-[50px]">
-      <div className="h-max w-full flex flex-col items-center">
+      <div className="h-max w-full flex flex-col items-center max-[1080px]:items-start">
         <div className="h1">Contact Us</div>
         <div className="h-max w-full mt-[40px] flex flex-row justify-center gap-[30px]">
-          <div className="h-max w-[40%] flex flex-col justify-start items-start gap-[10px]">
+          <div className="h-max w-[40%] flex flex-col justify-start items-start gap-[10px] max-[1080px]:w-full">
             {Object.entries(form).map((item) => {
               const onChange = (newValue) => {
                 setForm((cur) => ({
